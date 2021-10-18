82b3163f08c09f8a914251e03648c5d4a1feb502
Romain TREFAULT <romain.trefault@epita.fr>
2021-10-18 20:06:54 +0200

    working on flood filling

--- pixel_operations.c
+++
@@ -1,8 +0,0 @@
-#include <err.h>
-#include "pixel_operations.h"
-
-static inline Uint8 *pixel_ref(SDL_Surface *surf, unsigned x, unsigned y)
-{
-    int bpp = surf->format->BytesPerPixel;
-    return (Uint8 *)surf->pixels + y * surf->pitch + x * bpp;
-}
