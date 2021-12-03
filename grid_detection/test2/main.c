993c45e30ff0d6030b717c97d6117931db848fe0
L0c <locinone@protonmail.com>
2021-12-03 15:57:46 +0100

    hough works, fix leaks

--- grid_detection/test2/main.c
+++ grid_detection/test2/main.c
@@ -1,20 +1,20 @@
 #include "SDL/SDL.h"
-#include "SDL/SDL_image"
+#include "SDL/SDL_image.h"
 #include <math.h>
 //#include "hough_transform.h"
 #include "operations.h"
 //#include "rotate.h"
 #include <sys/stat.h>
 #include "hough.h"
-#include "tools.h"
+//#include "tools.h"
 #define pi 3.14159265359
 
 
 int main(void){
 
-	SDL_Surface * img = load_image("sudoku-grid.png");
+	//SDL_Surface * img = load_image("sudoku-grid.png");
 
-	edge_detection("sudoku-grid.bmp");
+	edge_detection("test_sudoku.jpeg");
 
 
 	return 0;
